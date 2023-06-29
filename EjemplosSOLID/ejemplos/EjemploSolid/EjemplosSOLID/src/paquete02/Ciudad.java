8ef9b07f7120a74d3a171542e844a781f3e7e395
veronica-7 <vettonica18@gmail.com>
2023-06-29 11:49:33 -0500

    SOLID

---
+++ EjemplosSOLID/ejemplos/EjemploSolid/EjemplosSOLID/src/paquete02/Ciudad.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete02;
+
+/**
+ *
+ * @author SALA I
+ */
+public class Ciudad {
+ String nombre;
+
+    public Ciudad(String n) {
+        nombre = n;
+    }
+ 
+ 
+}
